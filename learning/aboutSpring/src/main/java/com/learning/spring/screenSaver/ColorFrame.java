fec3863413d477307bb926bd72fea839f56787ec
Mariia Bystrova <mbystrova@devexperts.com>
2022-12-01 22:18:25 +0100

    wrong AppContext

---
+++ learning/aboutSpring/src/main/java/com/learning/spring/screenSaver/ColorFrame.java
@@ -0,0 +1,32 @@
+package com.learning.spring.screenSaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.HeadlessException;
+import java.util.Random;
+
+@Component
+public class ColorFrame extends JFrame {
+
+    @Autowired
+//    private Color color;
+    private ApplicationContext context;
+
+    public ColorFrame() throws HeadlessException {
+        setSize(200,200);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace(){
+        Random random = new Random();
+        setLocation(random.nextInt(1000), random.nextInt(500));
+//        getContentPane().setBackground(color);
+        getContentPane().setBackground(context.getBean(Color.class));
+        repaint();
+    }
+}
---
+++ learning/aboutSpring/src/main/java/com/learning/spring/screenSaver/Config.java
@@ -0,0 +1,30 @@
+package com.learning.spring.screenSaver;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.awt.Color;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "com.learning.spring.screenSaver")
+public class Config {
+
+    @Bean
+    @Scope("prototype")
+    public Color color(){
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
+        while (true){
+            applicationContext.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(50);
+        }
+    }
+}
