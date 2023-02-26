59710322da160a22b1acfa60dfe4a70c1fe25f4b
Mariia Bystrova <mbystrova@devexperts.com>
2023-02-26 23:47:44 +0100

    fix test

--- spring/src/test/java/learning/spring/stepik/externaltask/threebeans/FunnyTest.java
+++ spring/src/test/java/learning/spring/stepik/externaltask/threebeans/FunnyTest.java
@@ -5,6 +5,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
 public class FunnyTest {
     AnnotationConfigApplicationContext context;
 
@@ -21,13 +27,23 @@ public class FunnyTest {
     @Test
     public void testInjectMap() {
         FreeMap bean = context.getBean(FreeMap.class);
-        System.out.println("injected map = " + bean.getAbracadabraMap());
+        Map<String, Abracadabra> abracadabraMap = bean.getAbracadabraMap();
+        assertEquals(2, abracadabraMap.size());
+        List<Map.Entry<String, Abracadabra>> result = abracadabraMap.entrySet()
+            .stream()
+            .filter(e -> DefunctAbracadabra.class.equals(e.getValue().getClass()) || ExistingAbracadabra.class.equals(e.getValue().getClass()))
+            .collect(Collectors.toList());
+        assertEquals(2, result.size());
     }
 
     @Test
     public void testInjectList() {
         FreeList bean = context.getBean(FreeList.class);
-        System.out.println("injected list = " + bean.getAbracadabraList());
+        List<Abracadabra> abracadabraList = bean.getAbracadabraList();
+        assertEquals(2, abracadabraList.size());
+        List<Abracadabra> result = abracadabraList.stream()
+            .filter(e -> DefunctAbracadabra.class.equals(e.getClass()) || ExistingAbracadabra.class.equals(e.getClass()))
+            .collect(Collectors.toList());
+        assertEquals(2, result.size());
     }
-
 }
